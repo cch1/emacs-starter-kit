fef312d9d98ddcc57ed0c899632d94a7a8f2bf26
Chris Hapgood <cch1@hapgoods.com>
2014-02-03 14:40:50 -0500

    Collect dired stuff and evaluate early to avoid errors
    It appears that dired is used by some other startup commands. Further-
    more, it is broken under OSX in certain modes.  See this thread for
    details:
    
    	http://stackoverflow.com/questions/4076360/error-in-dired-sorting-on-os-x

--- init.el
+++ init.el
@@ -1,3 +1,13 @@
+;; (put 'dired-find-alternate-file 'disabled nil)
+(when (eq system-type 'darwin)
+  ;; (require 'ls-lisp)
+  ;; (setq ls-lisp-use-insert-directory-program nil)
+  ;; Either the above two commands or the following three commands should
+  ;; be used on darwin due to incompatibilities with the built-in ls command.
+  (setq insert-directory-program "/usr/local/bin/gls")
+  (setq dired-listing-switches "-aBhl --group-directories-first")
+  (setq dired-use-ls-dired nil))
+
 (x-focus-frame nil) ;; open window in front of terminal
 (require 'package)
 (add-to-list 'package-archives
@@ -37,8 +47,6 @@
 
 ;; (add-hook 'window-setup-hook 'maximize-frame t)
 
-;; (put 'dired-find-alternate-file 'disabled nil)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -46,7 +54,6 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-enabled-themes (quote (manoj-dark)))
- '(dired-use-ls-dired nil)
  '(inhibit-startup-screen t)
  '(slime-net-coding-system (quote utf-8-unix)))
 (custom-set-faces
