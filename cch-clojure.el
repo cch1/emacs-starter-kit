c5b99c5eefea2305dced19c7ae9a18f3433ac26f
Chris Hapgood <cch1@hapgoods.com>
2014-02-03 21:25:09 -0500

    Take a stab at auto-complete with ac-nrepl

--- cch-clojure.el
+++ cch-clojure.el
@@ -2,6 +2,8 @@
   '(paredit
     highlight-parentheses
     clojure-mode
+    auto-complete
+    ac-nrepl
     cider
     midje-mode)
   "A list of packages to ensure are installed at launch.")
@@ -9,6 +11,12 @@
 (dolist (p my-clojure-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+;; https://github.com/clojure-emacs/ac-nrepl
+(require 'ac-nrepl)
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'cider-repl-mode))
 
 (add-hook 'clojure-mode-hook
           (lambda ()
@@ -19,6 +27,7 @@
             (paredit-mode t)))
 
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
 
 ;; CIDER mode settings
 (setq cider-repl-pop-to-buffer-on-connect nil)
