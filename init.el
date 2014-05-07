15e3dcc957e2de41b8d8747ee5e791465d07629c
Chris Hapgood <cch1@hapgoods.com>
2014-05-07 16:58:06 -0400

    Remove slime cruft

--- init.el
+++ init.el
@@ -23,8 +23,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (manoj-dark)))
- '(slime-net-coding-system (quote utf-8-unix)))
+ '(custom-enabled-themes (quote (manoj-dark))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
