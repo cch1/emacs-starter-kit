a89efefe7dad6d5dfd89a65a629a2c92b72b7a64
Chris Hapgood <cch1@hapgoods.com>
2015-12-15 17:07:43 -0500

    Update lisp editing behavior

--- lisp/cch-clojure.el
+++ lisp/cch-clojure.el
@@ -21,17 +21,21 @@
 (require 'pretty-symbols)
 (add-to-list 'pretty-symbol-patterns '(?ƒ lambda "\\<fn\\>" (clojure-mode)))
 
+(defun lisp-editing-behavior ()
+  (highlight-parentheses-mode t)
+  (idle-highlight-mode t)
+  (pretty-symbols-mode t)
+  (paredit-mode t)
+  (local-set-key (kbd "{") 'paredit-open-curly))
+
 (add-hook 'clojure-mode-hook
           (lambda ()
             ;; Trim trailing whitespace on write buffer.
             ;; Note that trailing newlines at the end of the file are NOT trimmed.
             (add-hook 'local-write-file-hooks (lambda () (save-excursion (delete-trailing-whitespace))))
-            (highlight-parentheses-mode t)
-	    (idle-highlight-mode t)
-	    (pretty-symbols-mode t)
-            (paredit-mode t)))
+	    (lisp-editing-behavior)))
 
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'lisp-editing-behavior)
 
 ;; CIDER mode settings
 (setq nrepl-hide-special-buffers t) ;; When using switch-to-buffer, pressing SPC after the command will make the hidden buffers visible.
